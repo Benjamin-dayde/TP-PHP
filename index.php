d7a9e8e01d2a5e4a968489da82183b73de73ab18
Benjamin-dayde <benjamin.dayde@orange.fr>
2020-04-09 14:04:31 +0200

    Parti 1

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+
+class Utilisateur {
+
+    insert_user()
+
+
+
+
+
+}
+
+class Tache {
+
+}
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Connection</title>
+    <link rel="stylesheet" type="text/css" href="cv.css">
+</head>
+<body>
+    <header id="entete">
+    </header>
+    <div id="encadre">
+        <article id="cadre">
+            <p>
+            <form method="post"action="index.php">
+                 <fieldset>
+                  <legend>information principale</legend>
+                    <label for="pseudo"> pseudo</label>
+                    <input type="text" name="pseudo" id="pseudo"placeholder="" required="required"/><br>
+                    <label for="pass">mot de passe</label>
+                    <input type="password"name="pass" id="pass"/>
+                 </fieldset>
+            </p>
+        </article>
+    </div>
+</body>
+</html>
